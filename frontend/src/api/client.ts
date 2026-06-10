4616a31dc063a6cbea9d5c78f2070e154e6af888
minsik1014 <ms03zzang@gmail.com>
2026-06-10 16:47:41 +0900

    fix: 프런트/백엔드 연동과정 오류 해결4

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -6,6 +6,7 @@ const client = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': 'true', // Ngrok 경고창 우회
   },
   timeout: 5000,
 });
